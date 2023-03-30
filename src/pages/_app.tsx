760621cbdf2fb3e73d65d8c710a34040940b04b1
Mustafoyev Ahmadjon <79581200+mustafoyevahmadjon@users.noreply.github.com>
2023-03-30 21:24:28 +0500

    create next app & config Material UI

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import createEmotionCache from '@/helpers/create-emotion-cashe'
+import theme from '@/helpers/theme'
+import '@/styles/globals.css'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
